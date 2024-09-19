80d70bcaf6d1a64fd10b4844bb6d2ca491298950
DESKTOP-4HLNNVS\TOSHIBA <quang.danhnhut@hcmut.edu.vn>
2024-09-20 00:45:43 +0700

    Update Ex1

--- Lab_2/Core/Inc/main.h
+++ Lab_2/Core/Inc/main.h
@@ -58,6 +58,36 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Lest_Test_All_Pin GPIO_PIN_1
+#define Lest_Test_All_GPIO_Port GPIOA
+#define Led_Test_Timer_Pin GPIO_PIN_2
+#define Led_Test_Timer_GPIO_Port GPIOA
+#define Dot_Pin GPIO_PIN_5
+#define Dot_GPIO_Port GPIOA
+#define Led_5_Pin GPIO_PIN_6
+#define Led_5_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_7
+#define EN0_GPIO_Port GPIOA
+#define EN_0_Pin GPIO_PIN_0
+#define EN_0_GPIO_Port GPIOB
+#define SEG1_Pin GPIO_PIN_1
+#define SEG1_GPIO_Port GPIOB
+#define SEG2_Pin GPIO_PIN_2
+#define SEG2_GPIO_Port GPIOB
+#define EN_1_Pin GPIO_PIN_8
+#define EN_1_GPIO_Port GPIOA
+#define EN_2_Pin GPIO_PIN_9
+#define EN_2_GPIO_Port GPIOA
+#define EN_3_Pin GPIO_PIN_10
+#define EN_3_GPIO_Port GPIOA
+#define SEG3_Pin GPIO_PIN_3
+#define SEG3_GPIO_Port GPIOB
+#define SEG4_Pin GPIO_PIN_4
+#define SEG4_GPIO_Port GPIOB
+#define SEG5_Pin GPIO_PIN_5
+#define SEG5_GPIO_Port GPIOB
+#define SEG6_Pin GPIO_PIN_6
+#define SEG6_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
